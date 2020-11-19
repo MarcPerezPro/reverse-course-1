b8cbd6d0696e2267bd62f40d2736377d8309bc9b
Killian Gardahaut <killian.gardahaut@epitech.eu>
2020-11-19 17:30:12 +0100

    Update hack.sh

--- ELF_FAST/ELF_FAST_02/hack.sh
+++ ELF_FAST/ELF_FAST_02/hack.sh
@@ -1,9 +1,7 @@
 wget reverse.blackfoot.io:8080/ELF_02
 flag=`strings ELF_02 | grep -o -w '\w\{20\}' | grep -v deregister_tm_clones`
-echo $flag
 flagModif=`python3 hack.py $flag`
 
-echo "$flagModif"
 url="http://reverse.blackfoot.io:8080/validate/ELF_02/$flagModif"
 curl -X GET $url
-#rm ELF_02
+rm ELF_02
